49191cc180786dc5e3d5a3e1e65e5e61af7546d9
kmturhan <kmturhan@gmail.com>
2023-02-15 02:38:42 +0300

    inputs get and changed value

--- src/index.js
+++ src/index.js
@@ -1,44 +1,20 @@
-import React from 'react';
+import { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
-class ShoppingList extends React.Component {
-  render() {
-    return (
-      <div className="shopping-list">
-        <h1>Shopping List for {this.props.name}</h1>
-        <ul>
-          <li>Instagram</li>
-          <li>WhatsApp</li>
-          <li>Oculus</li>
-        </ul>
-      </div>
-    );
+
+
+function MyForm() {
+  const [name, setName] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("test",event)
+    event.target[1].value = 9999;
   }
-}
 
-
-
-class Table extends React.Component {
-  render() {
-    function handleSubmit(e) {
-      // Prevent the browser from reloading the page
-      e.preventDefault();
-
-      // Read the form data
-      const form = e.target;
-      const formData = new FormData(form);
-
-      // You can pass formData as a fetch body directly:
-      fetch('/some-api', { method: form.method, body: formData });
-
-      // Or you can work with it as a plain object:
-      const formJson = Object.fromEntries(formData.entries());
-      console.log(formJson);
-    }
-    return (
-
-      <div className='table' onSubmit={handleSubmit}>
+  return (
+    <form className='table' onSubmit={handleSubmit}>
         <h1>SUDO-SOLVER</h1>
         <div className='table-wrapper'>
           <div className='table-row'>
@@ -46,49 +22,41 @@ class Table extends React.Component {
               const arr = [];
               for (let index = 1; index < 9; index++) {
                 arr.push(
-                  <input inputMode='numeric' type="number" className={`table-row-cell table-row-cell-${index}`} onKeyDown={(evt) => evt.key === 'e' && evt.preventDefault()}></input>
+                  <input key={"row1"+index} inputMode='numeric' type="number" className={`table-row-cell table-row1-cell-${index}`} onKeyDown={(evt) => evt.key === 'e' && evt.preventDefault()} onChange={(e) => setName(e.target.value)}></input>
                 );
               }
               return arr;
             })()}
-
-
-            <input inputMode='numeric' type="number" className='table-row-cell' onKeyDown={(evt) => evt.key === 'e' && evt.preventDefault()}></input>
-            <input inputMode='numeric' type="number" className='table-row-cell' onKeyDown={(evt) => evt.key === 'e' && evt.preventDefault()}></input>
-            <input inputMode='numeric' type="number" className='table-row-cell' onKeyDown={(evt) => evt.key === 'e' && evt.preventDefault()}></input>
-            <input inputMode='numeric' type="number" className='table-row-cell' onKeyDown={(evt) => evt.key === 'e' && evt.preventDefault()}></input>
-            <input inputMode='numeric' type="number" className='table-row-cell' onKeyDown={(evt) => evt.key === 'e' && evt.preventDefault()}></input>
-            <input inputMode='numeric' type="number" className='table-row-cell' onKeyDown={(evt) => evt.key === 'e' && evt.preventDefault()}></input>
-            <input inputMode='numeric' type="number" className='table-row-cell' onKeyDown={(evt) => evt.key === 'e' && evt.preventDefault()}></input>
-            <input inputMode='numeric' type="number" className='table-row-cell' onKeyDown={(evt) => evt.key === 'e' && evt.preventDefault()}></input>
-            <input inputMode='numeric' type="number" className='table-row-cell' onKeyDown={(evt) => evt.key === 'e' && evt.preventDefault()}></input>
           </div>
           <div className='table-row'>
-            <input inputMode='numeric' type="number" className='table-row-cell' onKeyDown={(evt) => evt.key === 'e' && evt.preventDefault()}></input>
-            <input inputMode='numeric' type="number" className='table-row-cell' onKeyDown={(evt) => evt.key === 'e' && evt.preventDefault()}></input>
-            <input inputMode='numeric' type="number" className='table-row-cell' onKeyDown={(evt) => evt.key === 'e' && evt.preventDefault()}></input>
-            <input inputMode='numeric' type="number" className='table-row-cell' onKeyDown={(evt) => evt.key === 'e' && evt.preventDefault()}></input>
-            <input inputMode='numeric' type="number" className='table-row-cell' onKeyDown={(evt) => evt.key === 'e' && evt.preventDefault()}></input>
-            <input inputMode='numeric' type="number" className='table-row-cell' onKeyDown={(evt) => evt.key === 'e' && evt.preventDefault()}></input>
-            <input inputMode='numeric' type="number" className='table-row-cell' onKeyDown={(evt) => evt.key === 'e' && evt.preventDefault()}></input>
-            <input inputMode='numeric' type="number" className='table-row-cell' onKeyDown={(evt) => evt.key === 'e' && evt.preventDefault()}></input>
-            <input inputMode='numeric' type="number" className='table-row-cell' onKeyDown={(evt) => evt.key === 'e' && evt.preventDefault()}></input>
+          {(() => {
+              const arr = [];
+              for (let index = 1; index < 9; index++) {
+                arr.push(
+                  <input key={"row1"+index} inputMode='numeric' type="number" className={`table-row-cell table-row2-cell-${index}`} onKeyDown={(evt) => evt.key === 'e' && evt.preventDefault()} onChange={(e) => setName(e.target.value)}></input>
+                );
+              }
+              return arr;
+            })()}
           </div>
           <div className='table-row'>
-            <input inputMode='numeric' type="number" className='table-row-cell' onKeyDown={(evt) => evt.key === 'e' && evt.preventDefault()}></input>
-            <input inputMode='numeric' type="number" className='table-row-cell' onKeyDown={(evt) => evt.key === 'e' && evt.preventDefault()}></input>
-            <input inputMode='numeric' type="number" className='table-row-cell' onKeyDown={(evt) => evt.key === 'e' && evt.preventDefault()}></input>
-            <input inputMode='numeric' type="number" className='table-row-cell' onKeyDown={(evt) => evt.key === 'e' && evt.preventDefault()}></input>
-            <input inputMode='numeric' type="number" className='table-row-cell' onKeyDown={(evt) => evt.key === 'e' && evt.preventDefault()}></input>
-            <input inputMode='numeric' type="number" className='table-row-cell' onKeyDown={(evt) => evt.key === 'e' && evt.preventDefault()}></input>
-            <input inputMode='numeric' type="number" className='table-row-cell' onKeyDown={(evt) => evt.key === 'e' && evt.preventDefault()}></input>
-            <input inputMode='numeric' type="number" className='table-row-cell' onKeyDown={(evt) => evt.key === 'e' && evt.preventDefault()}></input>
-            <input inputMode='numeric' type="number" className='table-row-cell' onKeyDown={(evt) => evt.key === 'e' && evt.preventDefault()}></input>
+          {(() => {
+              const arr = [];
+              for (let index = 1; index < 9; index++) {
+                arr.push(
+                  <input key={"row1"+index} inputMode='numeric' type="number" className={`table-row-cell table-row3-cell-${index}`} onKeyDown={(evt) => evt.key === 'e' && evt.preventDefault()} onChange={(e) => setName(e.target.value)}></input>
+                );
+              }
+              return arr;
+            })()}
           </div>
         </div>
-      </div>
-    )
-  }
+        <button type='submit'>test</button>
+      </form>
+
+  )
 }
+
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Table />);
+root.render(<MyForm />);
